c6df9e24a471f5d99e1ab11048ff92eb56658de7
Linh Nguyen <npl02042000@gmail.com>
2023-02-05 00:48:27 -0500

    Update allBook.tsx

--- front/src/pages/allBook.tsx
+++ front/src/pages/allBook.tsx
@@ -1,11 +1,13 @@
 import Books from '../components/Books';
 import Book from '../components/Book';
+import BookUpdateDelete from '../components/BookUpdateDelete';
 
 function AllBook() {
   return (
     <div className="container">
       <Books />
       <Book/>
+      <BookUpdateDelete/>
     </div>
   )
 }
