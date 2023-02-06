737025e9c89ac04d08fc43cd2d810057d6d63116
Linh Nguyen <npl02042000@gmail.com>
2023-02-05 23:55:21 -0500

    Update layout.tsx

--- front/src/pages/layout.tsx
+++ front/src/pages/layout.tsx
@@ -1,15 +1,20 @@
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import { Outlet, Link } from "react-router-dom";
 
-const Layout = () => {
+function Layout() {
   return (
     <>
       <nav className="background-color: #f45d49 nav-class">
-        < Link to="/">Home</Link>  |  <Link to="/authors">Authors</Link>  |  <Link to="/books">Books</Link>
+      <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" /> < Link to="/">Home</Link> 
+      | <AccountCircleIcon sx={{ mr: 0.5 }} fontSize="inherit" /> <Link to="/authors">Authors</Link>  
+      | <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" /> <Link to="/books">Books</Link>
       </nav>
 
       <Outlet />
     </>
-  )
-};
+  );
+}
 
 export default Layout;
