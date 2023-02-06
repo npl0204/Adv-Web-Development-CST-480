8b1d5605c4ecdee1c6075a031360145aac82eeda
Linh Nguyen <npl02042000@gmail.com>
2023-02-05 23:55:41 -0500

    Update main.tsx

--- front/src/pages/main.tsx
+++ front/src/pages/main.tsx
@@ -3,15 +3,19 @@ import Layout from "./layout";
 import Home from "./home";
 import AllAuthor from "./allAuthor";
 import AllBook from "./allBook";
+import UpdateBook from "../components/UpdateBook";
+import UpdateAuthor from "../components/UpdateAuthor";
 
 function Main() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
           <Route path="/authors" element={<AllAuthor />} />
+          <Route path="/authors/:id" element={<UpdateAuthor />}/>
           <Route path="/books" element={<AllBook />} />
+          <Route path="/books/:id" element={<UpdateBook />}/>
         </Route>
       </Routes>
     </HashRouter>
