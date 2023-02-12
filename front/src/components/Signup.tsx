e6efcfc61dbbe295f6f438e36a25fddeef334d47
Linh Nguyen <npl02042000@gmail.com>
2023-02-11 22:38:33 -0500

    Add files via upload

---
+++ front/src/components/Signup.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Box,
+  Avatar,
+  Grid
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+function Signup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('Not logged in');
+
+  function signup(e: React.FormEvent) {
+    e.preventDefault();
+    axios.post('/signup', {
+      username: username,
+      password: password
+    }).then((res) => {
+      const data = res.data;
+      localStorage.setItem('token', data.token);
+      setMessage(data.message);
+    }).catch((error) => {
+      let errorObj = error as AxiosError;
+      if (errorObj.response === undefined) {
+        throw errorObj;
+      }
+      let { response } = errorObj;
+      console.log(response.data);
+    })
+  }
+
+  return (
+    <>
+      <Container component="main" sx={{ mt: 8, mb: 5 }} maxWidth="xs">
+        <Typography variant="h5" color="lightpink"><b>{ message }</b></Typography>
+        <Box
+          component="form"
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#fd8496' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <div>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="outlined-required"
+                label="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="outlined-required"
+                type="password"
+                label="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Box textAlign='center'>
+                <Button variant="outlined" onClick={signup} sx={{ color: '#fd8496', mt: 3, mb: 2 }}>
+                  Signup
+                </Button>
+              </Box>
+              <Grid container>
+                <Grid item>
+                  <Link to={"/"} style={{ color: '#fd8496', textDecoration: 'inherit' }}>
+                    <p>
+                      <span className="normal">Already have an account? Sign in</span>
+                    </p>
+                  </Link>
+                </Grid>
+              </Grid>
+            </div>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default Signup;
