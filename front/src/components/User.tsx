f215b93e72818f7e37dd75ed24b19846a469c429
Linh Nguyen <npl02042000@gmail.com>
2023-02-11 22:39:10 -0500

    Add files via upload

---
+++ front/src/components/User.tsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
+import {
+  TextField,
+  Button,
+  Container,
+  Divider,
+  Typography,
+  Box,
+  Avatar,
+  Grid
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
+
+function User() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('Not logged in');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(token) {
+      setToken(token);
+      setMessage('Logged in');
+    } else {
+      setMessage('Not authorized');
+    }
+  }, [token]);
+
+  function login(e: React.FormEvent) {
+    e.preventDefault();
+    axios.post('/login', {
+      username: username,
+      password: password
+    }).then((res) => {
+      const data = res.data;
+      localStorage.setItem('token', data.token);
+      setMessage(data.message);
+      window.location.reload();
+    }).catch((error) => {
+      let errorObj = error as AxiosError;
+      if (errorObj.response === undefined) {
+        throw errorObj;
+      }
+      let { response } = errorObj;
+      console.log(response.data);
+    })
+  }
+
+  function logout(e: React.FormEvent) {
+    e.preventDefault();
+    axios.post('/logout').then((res) => {
+      console.log(res.data);
+      alert("You have been logged out");
+      setMessage(res.data.message);
+      window.location.reload();
+    })
+    localStorage.removeItem('token');
+  }
+
+  function signup(e: React.FormEvent) {
+    e.preventDefault();
+    axios.post('/signup', {
+      username: username,
+      password: password
+    }).then((res) => {
+      const data = res.data;
+      localStorage.setItem('token', data.token);
+      setMessage(data.message);
+      window.location.reload();
+    }).catch((error) => {
+      let errorObj = error as AxiosError;
+      if (errorObj.response === undefined) {
+        throw errorObj;
+      }
+      let { response } = errorObj;
+      console.log(response.data);
+    })
+  }
+
+  if (token.length > 0) {
+    return (
+      <>
+        <Divider />
+        <Container component="main" sx={{ mt: 5, mb: 5 }} maxWidth="xs">
+          <Typography variant="h5" color="lightpink"><b>{ message }</b></Typography>
+          <Box textAlign='center'>
+            <Button variant="outlined" onClick={logout} sx={{ color: '#fd8496', mt: 3, mb: 2 }}>
+            Logout
+            </Button>
+          </Box>
+        </Container>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Divider />
+      <Container component="main" sx={{ mt: 5, mb: 5 }} maxWidth="xs">
+        <Typography variant="h5" color="lightpink"><b>{ message }</b></Typography>
+        <Box
+          component="form"
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#fd8496' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <div>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="outlined-required"
+                label="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="outlined-required"
+                type="password"
+                label="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Box textAlign='center'>
+                <Button variant="outlined" onClick={login} sx={{ color: '#fd8496', mt: 3, mb: 2 }}>
+                  Login
+                </Button>
+              </Box>
+              <Grid container>
+                <Grid item>
+                  <Link to={"/signup"} style={{ color: '#fd8496', textDecoration: 'inherit' }}>
+                    <p>
+                      <span className="normal">Don't have an account? Sign Up</span>
+                    </p>
+                  </Link>
+                </Grid>
+              </Grid>
+            </div>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default User;
