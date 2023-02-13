c9f50b791974942a94ace978c8d5188cce276e40
Linh Nguyen <npl02042000@gmail.com>
2023-02-13 11:40:06 -0500

    Update test-data.sql

--- test-data.sql
+++ test-data.sql
@@ -12,3 +12,13 @@ INSERT INTO books (id, author_id, title, pub_year, genre) VALUES (3, 3, 'The Pla
 INSERT INTO books (id, author_id, title, pub_year, genre) VALUES (4, 4, 'The Adventures of Tom Sawyer', 1876, 'Novel');
 INSERT INTO books (id, author_id, title, pub_year, genre) VALUES (5, 5, 'The Hookup Plan', 2022, 'Fantasy');
 INSERT INTO books (id, author_id, title, pub_year, genre) VALUES (6, 4, 'Personal Memoirs of U. S. Grant', 1885, 'Autobiography');
+
+-- Insert password and usernaem into users table
+-- password
+INSERT INTO users(username, password, role) VALUES ('admin', '$argon2id$v=19$m=65536,t=3,p=4$0toyJJQ6Xdv5rUQq1cCoCQ$hYs/2qQrQDy4gld9v4fy0kiQnBzpAu/FWyJgyTq3Ito', 'admin');
+-- abc
+INSERT INTO users(username, password, role) VALUES ('applesauce', '$argon2id$v=19$m=65536,t=3,p=4$aet/Up/t2f9Bu8teKj5SZA$KTYJ35q136nHVyphnqR3Zs9an5gS0hn1inw5YUoi8TU', 'user');
+-- fiddlesticks
+INSERT INTO users(username, password, role) VALUES ('bananabread', '$argon2id$v=19$m=65536,t=3,p=4$KwUDBdwmyFhYtFmdiUI+Nw$aVYp48DsGXYrBYMELdUbj4iO89eS8BwOhK9OPhYHQOE', 'user');
+-- correcthorsebatterystaple
+INSERT INTO users(username, password, role) VALUES ('coconutcake', '$argon2id$v=19$m=65536,t=3,p=4$A2+TzjOmpPShDeSaGiBjEg$zf1NFDQYKZWHXhge/f9ZhjrmmnzQ8v8q4UWHXX75SMI', 'admin');
