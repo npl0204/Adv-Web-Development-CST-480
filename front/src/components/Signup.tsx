105952c0c801ebf41085b6dd250a798f4415e73a
Linh Nguyen <npl02042000@gmail.com>
2023-02-12 20:54:49 -0500

    Update Signup.tsx

--- front/src/components/Signup.tsx
+++ front/src/components/Signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios, { AxiosError } from 'axios';
 import {
   TextField,
@@ -9,13 +9,24 @@ import {
   Avatar,
   Grid
 } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { MessageResponse } from '../../src/type';
 
 function Signup() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('Not logged in');
+  const [token, setToken] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(token) {
+      setToken(token);
+      setMessage('Logged in');
+    }
+  }, [token]);
 
   function signup(e: React.FormEvent) {
     e.preventDefault();
@@ -26,14 +37,27 @@ function Signup() {
       const data = res.data;
       localStorage.setItem('token', data.token);
       setMessage(data.message);
+      alert("Sign up successful! You are logged in");
+      navigate("/"); // Redirect to the sign-in page
     }).catch((error) => {
       let errorObj = error as AxiosError;
       if (errorObj.response === undefined) {
         throw errorObj;
       }
       let { response } = errorObj;
-      console.log(response.data);
+      let data = response.data as MessageResponse;
+      alert(data.message);
     })
+  }
+
+  if(token.length > 0) {
+    return (
+      <>
+        <Container component="main" sx={{ mt: 8, mb: 5 }} maxWidth="xs">
+          <Typography variant="h5" color="lightpink"><b>{ message }</b></Typography>
+        </Container>
+      </>
+    )
   }
 
   return (
