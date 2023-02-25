5aaa47dce8e9619824afe5784b5e82a48dea03db
Linh Nguyen <npl02042000@gmail.com>
2023-02-25 12:50:46 -0500

    Update User.tsx

--- front/src/components/User.tsx
+++ front/src/components/User.tsx
@@ -11,7 +11,6 @@ import {
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { createTheme } from '@mui/material/styles';
 import { MessageResponse } from '../../src/type';
 
 function User() {
