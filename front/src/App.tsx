83f30923a06069b38b3e1d2b8f2a29b6dd57c57e
Linh Nguyen <npl02042000@gmail.com>
2023-02-25 12:14:16 -0500

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
 import Main from './pages/main';
 
+// @ts-ignore
+import {Helmet} from "react-helmet";
+
 function App() {
   return (
-    <Main />
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>CST480 - Nguyen Phuong Linh</title>
+        <link rel="canonical" href="https://hw5.linhpnguyen.com/" />
+      </Helmet>
+      <Main />
+    </div>
   )
 }
 
